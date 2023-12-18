78d4bc04b5339f196b9caac45839a3f4521c8bed
Said Alisic <saidalisic@Saids-MacBook-Pro.local>
2023-12-18 18:08:52 +0100

    feat: 🎸 Add ScrollView to main App component

--- App.tsx
+++ App.tsx
@@ -1,15 +1,15 @@
 import { StatusBar } from "expo-status-bar";
-import { View } from "react-native";
+import { ScrollView } from "react-native";
 import { styles } from "./styles/styles";
 import AppHeader from "./components/AppHeader";
 import Calendar from "./components/Calendar";
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <ScrollView style={styles.container}>
       <StatusBar style="auto" />
       <AppHeader />
       <Calendar />
-    </View>
+    </ScrollView>
   );
 }
