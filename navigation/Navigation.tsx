18f1ae9eff1e1ddc59e6021605ae3de499686222
Said Alisic <saidalisic@Saids-MacBook-Pro.local>
2024-01-08 19:24:34 +0100

    feat: 🎸 Replace existing Navigation with BottomNavigation
    Using BottomNavigation from React Native Paper to render screens based
    on the selected icon

--- navigation/Navigation.tsx
+++ navigation/Navigation.tsx
@@ -1,20 +1,55 @@
-import React from "react";
-import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import React, { useState } from "react";
+import { BottomNavigation } from "react-native-paper";
+import { AppHeader } from "../components";
 import { CalendarScreen } from "../screens";
-
-const Stack = createNativeStackNavigator();
+import { bottomNavigationStyles } from "../styles/bottom-navigation-styles";
 
 export default function Navigation(): JSX.Element {
+  const [index, setIndex] = useState(0);
+  const [routes] = useState([
+    {
+      key: "calendar",
+      title: "Calendar",
+      focusedIcon: "calendar-month",
+      unfocusedIcon: "calendar-month-outline",
+    },
+    {
+      key: "settings",
+      title: "Settings",
+      focusedIcon: "cog",
+      unfocusedIcon: "cog-outline",
+    },
+  ]);
+
+  // Determines which screen to render based on the button selected in the navigation bar
+  const renderScene = BottomNavigation.SceneMap({
+    calendar: () => <CalendarScreen />,
+    settings: () => <AppHeader headerText="Plany" subText="Settings" />,
+  });
+
+  // Styling for the BottomNavigation component props
+  const navigationStyles = {
+    activeColor: "#000000",
+    inactiveColor: "#ffffff",
+    theme: {
+      colors: { secondaryContainer: "#ffffff" },
+    },
+  };
+
   return (
-    <NavigationContainer>
-      <Stack.Navigator
-        screenOptions={{
-          headerShown: false,
-        }}
-      >
-        <Stack.Screen name="Calendar" component={CalendarScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <BottomNavigation
+      // NOTE: `navigationState` is deprecated seems to only show due to the `color` option being deprecated in theme V3,
+      // otherwise it is still in use in the latest version of React Native Paper
+      navigationState={{ index, routes }}
+      onIndexChange={setIndex}
+      renderScene={renderScene}
+      labeled={false}
+      sceneAnimationEnabled={true}
+      sceneAnimationType="shifting"
+      barStyle={bottomNavigationStyles.barStyle} // Set the background color
+      activeColor={navigationStyles.activeColor} // Set the active tab color
+      inactiveColor={navigationStyles.inactiveColor} // Set the inactive tab color
+      theme={navigationStyles.theme}
+    />
   );
 }
