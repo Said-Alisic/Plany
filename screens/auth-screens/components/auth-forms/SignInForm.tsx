c1976e1020d82c01daf0e190f0eca30efa07d2ed
Said Alisic <saidalisic@Saids-MacBook-Pro.local>
2024-01-22 13:53:09 +0100

    refactor: 💡 Change button ripple effect colours

--- screens/auth-screens/components/auth-forms/SignInForm.tsx
+++ screens/auth-screens/components/auth-forms/SignInForm.tsx
@@ -6,6 +6,7 @@ import { useNavigation } from "@react-navigation/native";
 import { NativeStackNavigationProp } from "@react-navigation/native-stack";
 import { RootStackParamList } from "../../../../common/types";
 import { authFormStyles } from "../../../../styles/auth-styles/auth-form-styles";
+import { styles } from "../../../../styles/styles";
 
 export default function SignInForm(): JSX.Element {
   const [email, setEmail] = useState("");
@@ -18,7 +19,7 @@ export default function SignInForm(): JSX.Element {
   const handleSignIn = () => {
     if (email && password) {
       console.log(
-        `Logged in with email "${email}" and password "${password}".`,
+        `Logged in with email "${email}" and password "${password}".`
       );
 
       navigation.navigate("Root", { screen: "Calendar" });
@@ -53,6 +54,7 @@ export default function SignInForm(): JSX.Element {
       <Button
         contentStyle={authFormStyles.buttonContent}
         textColor="#ffffff"
+        rippleColor={styles.buttonRipple.color}
         mode="contained"
         onPress={handleSignIn}
         style={authFormStyles.button}
